55acf941b4ada784e7ca6aadd16b1e3945a52e74
mbortolin78-sys <m.bortolin78@gmail.com>
2025-10-30 17:23:04 +0100

    Delete modelli directory

--- modelli/auroria.js
+++
@@ -1,9 +0,0 @@
-async function calcola(data, ora, luogo, command) {
-  return {
-    oraria: { ascendente: "Leone", casa: "VII" },
-    galassia: { nome: "Andromeda", stella: "Taurus" },
-    carte: ["Il Sole", "La Temperanza", "Il Bagatto"],
-  };
-}
-
-module.exports = { calcola };
