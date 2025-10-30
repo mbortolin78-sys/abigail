7e2f61cb4dd5e41fc6b5fa3eb3f0cb744e2fdf4d
mbortolin78-sys <m.bortolin78@gmail.com>
2025-10-30 15:07:04 +0100

    Create auroria.js

---
+++ modelli/auroria.js
@@ -0,0 +1,9 @@
+async function calcola(data, ora, luogo, command) {
+  return {
+    oraria: { ascendente: "Leone", casa: "VII" },
+    galassia: { nome: "Andromeda", stella: "Taurus" },
+    carte: ["Il Sole", "La Temperanza", "Il Bagatto"],
+  };
+}
+
+module.exports = { calcola };
